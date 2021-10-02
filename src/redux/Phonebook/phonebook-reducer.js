d00efff7e5672e60077095915ca5c4f42a8acf21
Koliadjun <koliadjun@gmail.com>
2021-10-02 22:48:47 +0300

    refactor reducers

--- src/redux/Phonebook/phonebook-reducer.js
+++ src/redux/Phonebook/phonebook-reducer.js
@@ -1,16 +1,47 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 
-import { addContact, deleteContact, changeFilter } from './phonebook-action';
+import { changeFilter } from './phonebook-action';
+import {
+  fetchContacts,
+  addContact,
+  deleteContacts,
+} from './phonebook-operation';
 
 const items = createReducer([], {
-  [addContact]: (state, { payload }) => [...state, payload],
-  [deleteContact]: (state, { payload }) =>
+  [fetchContacts.fulfilled]: (_, { payload }) => payload,
+  [addContact.fulfilled]: (state, { payload }) => [...state, payload],
+  [deleteContacts.fulfilled]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
+});
+
+const isLoading = createReducer(false, {
+  [fetchContacts.pending]: () => true,
+  [fetchContacts.fulfilled]: () => false,
+  [fetchContacts.rejected]: () => false,
+
+  [addContact.pending]: () => true,
+  [addContact.fulfilled]: () => false,
+  [addContact.rejected]: () => false,
+
+  [deleteContacts.pending]: () => true,
+  [deleteContacts.fulfilled]: () => false,
+  [deleteContacts.rejected]: () => false,
 });
 
 const filter = createReducer('', {
   [changeFilter]: (_, { payload }) => payload,
 });
 
-export default combineReducers({ items, filter });
+const error = createReducer('', {
+  [fetchContacts.rejected]: (_, { payload }) => payload,
+  [fetchContacts.pending]: () => null,
+
+  [addContact.rejected]: (_, { payload }) => payload,
+  [addContact.pending]: () => null,
+
+  [deleteContacts.rejected]: (_, { payload }) => payload,
+  [deleteContacts.pending]: () => null,
+});
+
+export default combineReducers({ items, isLoading, error, filter });
