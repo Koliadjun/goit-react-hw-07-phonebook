1b05d36a6ffa90d80a4020e8ed128e60ca2cf80f
Koliadjun <koliadjun@gmail.com>
2021-10-02 23:20:14 +0300

    add мемоизацию

--- src/redux/Phonebook/phonebook-selectors.js
+++ src/redux/Phonebook/phonebook-selectors.js
@@ -1,9 +1,25 @@
-export const getFilteredData = state => {
-  const { items } = state.contacts;
-  const normalizeFilter = getFilterValue(state).toLowerCase();
-  return items.filter(contact =>
-    contact.name.toLowerCase().includes(normalizeFilter),
-  );
-};
+import { createSelector } from 'reselect';
+
+export const getItems = state => state.contact.items;
 
 export const getFilterValue = state => state.contacts.filter;
+
+export const getLoading = state => state.contact.isLoading;
+
+export const getFilteredData = createSelector(
+  [getItems, getFilterValue],
+  (items, filter) => {
+    const normalizeFilter = filter.toLowerCase();
+    return items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter),
+    );
+  },
+);
+
+// export const getFilteredData = state => {
+//     const items = getItems(state);
+//     const normalizeFilter = getFilterValue(state).toLowerCase();
+//     return items.filter(contact =>
+//         contact.name.toLowerCase().includes(normalizeFilter),
+//     );
+// };
